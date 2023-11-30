58bde1c821f21a327fac4bcc2afcf6ff40e0e232
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2023-11-30 21:46:11 +0100

    drawing circles

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.fillStyle = "white";
-ctx.fillRect(10, 10, 150, 150);
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+ctx.fillStyle = "yellow";
+ctx.strokeStyle = "red";
+ctx.lineWidth = 15;
+ctx.beginPath();
+ctx.arc(100, 150, 50, 0, Math.PI * 2);
+ctx.fill();
+ctx.stroke();
