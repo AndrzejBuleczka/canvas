81af2fb29cfdb2feb157d818481f0f32e9577b6b
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2023-11-30 22:23:20 +0100

    add partcile movement effect

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector("#canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particleArray = [];
 
 window.addEventListener("resize", function () {
   canvas.width = window.innerWidth;
@@ -16,29 +17,53 @@ const mouse = {
 canvas.addEventListener("click", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
-  // drawCircle();
 });
 
 canvas.addEventListener("mousemove", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
-  // drawCircle();
 });
 
-function drawCircle() {
-  ctx.fillStyle = "blue";
-  ctx.beginPath();
-  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-  ctx.fill();
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 2 - 1;
+    this.speedY = Math.random() * 2 - 1;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.fill();
+  }
 }
 
-class Particle {
-  
+function init() {
+  for (let i = 0; i < 100; i++) {
+    particleArray.push(new Particle());
+  }
+}
+
+init();
+
+function handleParticles() {
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+    particleArray[i].draw();
+  }
 }
 
 function animate() {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawCircle();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
   requestAnimationFrame(animate);
 }
 animate();
