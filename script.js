fe837b9decd883abb60ba8772a59fc99df55fe4b
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2023-12-01 08:53:56 +0100

    fix assignment

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const particleArray = [];
-const hue = 0;
+let hue = 0;
 
 window.addEventListener("resize", function () {
   canvas.width = window.innerWidth;
